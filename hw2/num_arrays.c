963ad9c502551a106994a91f4457de2bfdb509f4
Tarun Sivaraj <tsivaraj@ucsc.edu>
2022-01-31 02:38:05 +0000

    adding files from resources

--- hw2/num_arrays.c
+++ hw2/num_arrays.c
@@ -27,17 +27,58 @@ int sum_positive(int *nums, int len) {
 }
 
 int reduce(int *nums, int len, int (*f)(int,int), int initial){
-  return 0;
+    for(int i = 0; i < len; i++) {
+        initial = f(initial, nums[i]);
+    }
+    return initial;
 }
 
+int helper1(int first, int second) {
+    if (second > first) {
+        return second;
+    } else {
+        return first;
+    }
+    
+}
+
+int helper2(int first, int second) {
+    if (first > 0 && second > 0) {
+        return first + second;
+    } else {
+        if (first > 0) {
+            return first;
+        }
+        else if (second > 0) {
+            return second;
+        } else {
+            return 0;
+        }
+        
+    
+    }
+}
+
+int helper3(int first, int second) {
+    if (second < 0) {
+        first += 1;
+    }
+    return first;
+
+}
+
+
+
 int maximum_with_reduce(int *nums, int size) {
-  return 0;
+    return reduce(nums, size, helper1, INT_MIN);
 }
 
 int sum_positive_with_reduce(int *nums, int size) {
-  return 0;
+    return reduce(nums, size, helper2, 0);
+ 
 }
 
 int count_negative_with_reduce(int *nums, int size) {
-  return 0;
+    return reduce(nums, size, helper3, 0);
 }
+
