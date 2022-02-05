6eb8dc89a83da42dc2af58b0a345519da5774c5c
Tarun Sivaraj <tsivaraj@ucsc.edu>
2022-02-05 19:17:30 +0000

    got some more progress

--- hw3/stack_calculator.c
+++ hw3/stack_calculator.c
@@ -19,7 +19,7 @@ bool stack_push(Stack *s, CalculatorItem item) {
   if (s == NULL || (s->top == NULL)) {
     return false;
   }
-  n1->value = item;
+  n1->item = item;
   n1->next = s->top;
 
   s->top = n1;
@@ -30,23 +30,26 @@ bool stack_push(Stack *s, CalculatorItem item) {
 // Pop the stack and put the result into the output pointer.
 // return false if the stack is NULL or if it is empty.
 bool stack_pop(Stack *s, CalculatorItem *output) {
-  if (s == NULL || (s->top == NULL)) {
+  if (s == NULL || s->top == NULL) {
     return false;
   }
-  int result = s->top->value;
-  *output = result
+  CalculatorItem result = s->top->item;
+  *output = result;
 
-  Node *delete_this = s->top
-  free(delete_this)
+  Node *delete_this = s->top;
+  
+  s->top = s->top->next;
 
-  s->top = s->top->next
+  free(delete_this);
+
+  delete_this = NULL;
 
   return true;
 }
 
 // Returns true if the specified stack is empty.
 bool stack_empty(Stack *s) {
-  if(s->top == 0) {
+  if(s == NULL || s->top == NULL) {
     return true;
   }
   return false;
@@ -56,18 +59,80 @@ bool stack_empty(Stack *s) {
 // Free all of the memory used by this stack, including its linked list.
 // When you are done, set the pointer to NULL.
 void stack_delete(Stack **s) {
-  if (*s && (*s)->next) {
-    free((*s)->next);
-    free(*s);
-    *s = NULL;
+  while((*s)->top != NULL) {
+    Node *n2 = (*s)->top;
+    (*s)->top = (*s)->top->next;
+    free(n2);
+    n2 = NULL;
   }
-  return;
+  free(*s);
+  s = NULL;
 
 }
 
 // Returns true if we were able to successfully perform the computation step,
 // and false if there was an error of some kind.
 bool stack_compute_step(Stack *s, CalculatorItem item) {
-  // your code here
+  CalculatorItem x;
+  CalculatorItem y;
+  CalculatorItem res;
+
+  printf("%u\n", item.type);
+
+  if(item.type == ADD){
+    stack_pop(s, &item);
+    x.value = item.value;
+    printf("the x value: %f\n", x.value);
+    stack_pop(s, &item);
+    y.value = item.value;
+    printf("the y value: %f\n", y.value);
+    res.value = y.value + x.value;
+    res.type = NUMBER;
+    stack_push(s, res);
+    printf("%f\n", res.value);
+    printf("%u\n", res.type);
+  }
+  else if(item.type == SUBTRACT){
+    stack_pop(s, &item);
+    x.value = item.value;
+    printf("the x value: %f\n", x.value);
+    stack_pop(s, &item);
+    y.value = item.value;
+    printf("the y value: %f\n", y.value);
+    res.value = y.value - x.value;
+    stack_push(s, res);
+    printf("%f\n", res.value);
+  }
+  else if(item.type == MULTIPLY){
+    stack_pop(s, &item);
+    x.value = item.value;
+    printf("the x value: %f\n", x.value);
+    stack_pop(s, &item);
+    y.value = item.value;
+    printf("the y value: %f\n", y.value);
+    res.value = y.value * x.value;
+    stack_push(s, res);
+    printf("%f\n", res.value);
+  }
+  else if(item.type == DIVIDE){
+    stack_pop(s, &item);
+    x.value = item.value;
+    printf("the x value: %f\n", x.value);
+    stack_pop(s, &item);
+    y.value = item.value;
+    printf("the y value: %f\n", y.value);
+    res.value = y.value / x.value;
+    stack_push(s, res);
+    printf("%f\n", res.value);
+  }
+  else if (item.type == NUMBER){
+    //x.value = item.value;
+    printf("pushpushpush %f\n", item.value);
+    stack_push(s, item);
+    printf("pushpushpush %f\n", item.value);
+  }
+  else{
+    return false;
+  }
   return true;
 }
